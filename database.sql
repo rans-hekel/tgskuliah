75ff85511cb91cf83f6e4fa393b510966f1f37da
Rangga <72787297+rans-hekel@users.noreply.github.com>
2021-05-21 20:10:33 +0700

    menambahkan database

---
+++ database.sql
@@ -0,0 +1,65 @@
+#
+# TABLE STRUCTURE FOR: guru
+#
+
+DROP TABLE IF EXISTS `guru`;
+
+CREATE TABLE `guru` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (1, 'Anabelle Haley IV', '4556 Kertzmann Mills');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (2, 'Jovan Senger', '826 Krista Coves');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (3, 'Moses Langosh', '71865 Reichert Pines Suite 347');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (4, 'Constance Ratke', '080 Carroll Forges Apt. 185');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (5, 'Dr. Ova Robel', '66399 Molly Estates');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (6, 'Lionel Moore DVM', '5397 Durgan Key');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (7, 'Abraham Ruecker', '41936 Devonte Junctions Suite 678');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (8, 'Whitney Welch', '1734 Ofelia Stream');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (9, 'Abbigail Kihn', '009 Huel Points');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (10, 'Dereck Stiedemann', '1084 Boyle Fords Apt. 368');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (11, 'Boyd Rosenbaum', '700 Deckow Extension');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (12, 'Monserrate Frami Sr.', '4123 Pfannerstill Lodge Apt. 365');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (13, 'Dr. Abraham Paucek', '32673 Clement Courts Apt. 812');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (14, 'Clementina Witting', '171 Erwin Springs');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (15, 'Armand Turcotte', '097 Glennie Squares');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (16, 'Adelbert Luettgen I', '0544 Tristian Route');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (17, 'Aurelie Wiegand', '0265 Jimmy Ramp Suite 086');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (18, 'Dovie Goodwin', '4724 Judah Parks');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (19, 'Damon Cartwright', '770 Katrina Shoals');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (20, 'Renee Abshire', '061 Jedidiah Estates');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (21, 'Mr. Maxine Dare DDS', '360 Federico Rapids Suite 249');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (22, 'Ryleigh O\'Reilly', '2926 White Dale Apt. 902');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (23, 'Ray O\'Keefe PhD', '116 Macejkovic Avenue Apt. 503');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (24, 'Nicholaus Deckow', '279 Wisoky Villages');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (25, 'Mr. Julius Cummings', '437 Dolores Plaza');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (26, 'Dora Lemke', '67396 Botsford Lodge Apt. 461');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (27, 'Mellie Schmeler Jr.', '51371 Piper Isle');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (28, 'Donny Mueller', '10396 Eleazar Springs');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (29, 'Carissa Shields', '09444 Jordane Keys Apt. 184');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (30, 'Fredy Reichert', '237 Rosalinda Station Suite 106');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (31, 'Adrain Bosco', '219 Sally Stravenue');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (32, 'Hunter Kunze', '764 Kovacek Mountain');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (33, 'Zoila Hudson Jr.', '3336 Jovanny Forges');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (34, 'Kennedy Kovacek', '7475 Lonnie Wall Suite 489');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (35, 'Carissa Conn', '825 Howell Route');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (36, 'Ola Graham', '6283 Hahn Estate Suite 241');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (37, 'Garfield Hartmann', '72428 Daugherty Oval');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (38, 'Zechariah Macejkovic Sr.', '43073 Feil Camp');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (39, 'Laurence Cummerata', '6923 Deron Stream Apt. 208');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (40, 'Jackie Blanda', '349 Homenick Burgs Apt. 275');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (41, 'Ms. Melody Ondricka IV', '637 Kevon Fall');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (42, 'Zoie Wilderman', '720 Evert Freeway');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (43, 'Mrs. Aliyah Douglas', '96707 Mariano Streets Apt. 540');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (44, 'Prof. Lisandro Sawayn', '7301 Corene Stream');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (45, 'Mitchel Rempel', '86765 Kari Roads Suite 607');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (46, 'Cleta Stoltenberg DDS', '4501 Schmeler Neck Suite 112');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (47, 'Dr. Garrett Lang MD', '993 Annetta Bypass Apt. 515');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (48, 'Audrey Lubowitz', '21184 Stevie Ranch Suite 856');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (49, 'Kimberly Crist', '3391 Turcotte Fields');
+INSERT INTO `guru` (`id`, `nama`, `address`) VALUES (50, 'Jeremy Wilderman', '769 Blanda Shoal');
+
+
